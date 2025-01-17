5ccbc62af71d4d675e45f9a51efdbb0da15e7164
cesaredm <cesaredmaradiaga@outlook.com>
2025-01-16 20:31:54 -0600

    temas y codeium para ia de autocomplentado pero aun no funciona

--- lua/community.lua
+++ lua/community.lua
@@ -12,5 +12,7 @@ return {
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
   { import = "astrocommunity.recipes.cache-colorscheme" },
+  { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
   -- import/override with your plugins folder
 }
